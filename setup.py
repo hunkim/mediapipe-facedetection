efafa4bc1fee539ecc504c643033f6dc6026621b
Sung Kim <hunkim@gmail.com>
2023-04-01 23:51:17 +0900

    Aeed ip

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="mediapipe",
-    version="0.0.1",
-    packages=find_packages(),
+    version="0.1",
+    url="https://github.com/hunkim/streamlit-google-oauth",
+    author="Sung Kim",
+    author_email="hunkim@gmail.com",
+    py_modules=["mediapipe"],
+    packages=["mediapipe"],
     install_requires=[
-    ],
-    extras_require={
-    },
+       "numpy",
+    ]
 )
