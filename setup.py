8de065af4af648453e6c0837fe826e639368bae1
Sung Kim <hunkim@gmail.com>
2023-04-01 23:23:30 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 from setuptools import setup, find_packages
+
+from setuptools import setup, find_packages
+
+setup(
+    name="mediapipe",
+    version="0.0.1",
+    install_requires=[
+    ],
+    extras_require={
+    },
+)
