65f8880f01260a7c73776c70b371f99a185d57d4
Sung Kim <hunkim@gmail.com>
2023-04-01 22:45:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
-setupsetup(
+setup(
     name='mediapipe',
-    version=__version__,
+    version="0.0.1",
     url='https://github.com/google/mediapipe',
     description='MediaPipe is the simplest way for researchers and developers to build world-class ML solutions and applications for mobile, edge, cloud and the web.',
     author='The MediaPipe Authors',
