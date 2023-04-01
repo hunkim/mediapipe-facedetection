524994e8ce294b772faf7738b57009aaa43dc92b
Sung Kim <hunkim@gmail.com>
2023-04-01 23:33:00 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 setup(
     name="mediapipe",
     version="0.1",
     url="https://github.com/hunkim/streamlit-google-oauth",
     author="Sung Kim",
     author_email="hunkim@gmail.com",
-    packages=["mediapipe"],
-    package_data={"mediapipe": ["*"]},
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=[
         "numpy",
     ]
