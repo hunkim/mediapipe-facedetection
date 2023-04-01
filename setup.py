b8776fde81f992a0b1e1894a8eba76c9c35e931f
Sung Kim <hunkim@gmail.com>
2023-04-01 22:30:32 +0900

    Added Init

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mediapipie",
+    version="0.0.1",
+    packages=find_packages(),
+    install_requires=[
+    ],
+    extras_require={
+    },
+)
