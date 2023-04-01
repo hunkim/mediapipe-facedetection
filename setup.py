f75bc26b42fe26fd312ec606dc202454928a1a79
Sung Kim <hunkim@gmail.com>
2023-04-01 22:45:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,56 @@
 from setuptools import setup, find_packages
 
-setup(
-    name="mediapipie",
-    version="0.0.1",
-    packages=find_packages(),
-    install_requires=[
-    ],
-    extras_require={
+setupsetup(
+    name='mediapipe',
+    version=__version__,
+    url='https://github.com/google/mediapipe',
+    description='MediaPipe is the simplest way for researchers and developers to build world-class ML solutions and applications for mobile, edge, cloud and the web.',
+    author='The MediaPipe Authors',
+    author_email='mediapipe@google.com',
+    long_description=_get_long_description(),
+    long_description_content_type='text/markdown',
+    packages=setuptools.find_packages(
+        exclude=['mediapipe.examples.desktop.*', 'mediapipe.model_maker.*']),
+    install_requires=_parse_requirements('requirements.txt'),
+    cmdclass={
+        'build_py': BuildPy,
+        'build_modules': BuildModules,
+        'build_ext': BuildExtension,
+        'generate_metadata_schema': GenerateMetadataSchema,
+        'gen_protos': GeneratePyProtos,
+        'install': Install,
+        'restore': Restore,
     },
+    ext_modules=[
+        BazelExtension('//mediapipe/python:_framework_bindings'),
+        BazelExtension(
+            '//mediapipe/tasks/cc/metadata/python:_pywrap_metadata_version'),
+        BazelExtension(
+            '//mediapipe/tasks/python/metadata/flatbuffers_lib:_pywrap_flatbuffers'
+        ),
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    license='Apache 2.0',
+    keywords='mediapipe',
 )
